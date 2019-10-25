0055bf711861d4ee37edf3e20d2adfde2059b8df
Ryan <jh3155@gmail.com>
2019-10-24 20:20:17 -0400

    removed unused code

--- lets-do-it/src/App.vue
+++ lets-do-it/src/App.vue
@@ -21,18 +21,7 @@
       <router-view/>
     </v-content>
   </v-app>
-
-<!--
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
--->
 </template>
-
 
 <script>
 import SidebarMenu from './components/SidebarMenu';
