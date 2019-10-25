7b03fd45e879599a3d2bca747689139f118c4975
Ryan <jh3155@gmail.com>
2019-10-24 20:36:45 -0400

    improved task definition and updated display

--- lets-do-it/src/views/Tasks.vue
+++ lets-do-it/src/views/Tasks.vue
@@ -1,142 +1,105 @@
 <template>
   <v-container>
-    <v-layout
-      text-center
-      wrap
-    >
-      <v-flex xs12>
-        <v-img
-          :src="require('../assets/logo.svg')"
-          class="my-3"
-          contain
-          height="200"
-        ></v-img>
-      </v-flex>
+    <v-text-field
+      v-model="newTaskName"
+      label="Create a task !"
+      outlined
+      @keydown.enter="createTask"
+    ></v-text-field>
 
-      <v-flex mb-4>
-        <h1 class="display-2 font-weight-bold mb-3">
-          Welcome to Vuetify
-        </h1>
-        <p class="subheading font-weight-regular">
-          For help and collaboration with other Vuetify developers,
-          <br>please join our online
-          <a href="https://community.vuetifyjs.com" target="_blank">Discord Community</a>
-        </p>
-      </v-flex>
+    <v-list>
+      <v-subheader>TASKS</v-subheader>
 
-      <v-flex
-        mb-5
-        xs12
+      <v-list-item
+        v-for="task in tasks"
+        :key="task.id"
+        @click="createTask"
       >
-        <h2 class="headline font-weight-bold mb-3">What's next?</h2>
+        <template>
+          <v-list-item-action>
+            <v-checkbox
+              v-model="task.isCompleted"
+              color="success"
+            ></v-checkbox>
+          </v-list-item-action>
 
-        <v-layout justify-center>
-          <a
-            v-for="(next, i) in whatsNext"
-            :key="i"
-            :href="next.href"
-            class="subheading mx-3"
-            target="_blank"
+          <v-list-item-content>
+            <v-list-item-title v-text="task.name"></v-list-item-title>
+            <v-list-item-subtitle v-text="task.isImportant"></v-list-item-subtitle>
+          </v-list-item-content>
+
+          <v-list-item-action
+            @click="task.isImportant = !task.isImportant"
           >
-            {{ next.text }}
-          </a>
-        </v-layout>
-      </v-flex>
+            <v-icon
+              v-if="task.isImportant"
+              color="yellow"
+            >
+              star
+            </v-icon>
 
-      <v-flex
-        xs12
-        mb-5
-      >
-        <h2 class="headline font-weight-bold mb-3">Important Links</h2>
-
-        <v-layout justify-center>
-          <a
-            v-for="(link, i) in importantLinks"
-            :key="i"
-            :href="link.href"
-            class="subheading mx-3"
-            target="_blank"
-          >
-            {{ link.text }}
-          </a>
-        </v-layout>
-      </v-flex>
-
-      <v-flex
-        xs12
-        mb-5
-      >
-        <h2 class="headline font-weight-bold mb-3">Ecosystem</h2>
-
-        <v-layout justify-center>
-          <a
-            v-for="(eco, i) in ecosystem"
-            :key="i"
-            :href="eco.href"
-            class="subheading mx-3"
-            target="_blank"
-          >
-            {{ eco.text }}
-          </a>
-        </v-layout>
-      </v-flex>
-    </v-layout>
+            <v-icon
+              v-else
+              color="grey lighten-1"
+            >
+              star_border
+            </v-icon>
+          </v-list-item-action>
+        </template> 
+      </v-list-item>
+    </v-list>
   </v-container>
 </template>
 
 <script>
 export default {
-  data: () => ({
-    ecosystem: [
-      {
-        text: 'vuetify-loader',
-        href: 'https://github.com/vuetifyjs/vuetify-loader',
-      },
-      {
-        text: 'github',
-        href: 'https://github.com/vuetifyjs/vuetify',
-      },
-      {
-        text: 'awesome-vuetify',
-        href: 'https://github.com/vuetifyjs/awesome-vuetify',
-      },
-    ],
-    importantLinks: [
-      {
-        text: 'Documentation',
-        href: 'https://vuetifyjs.com',
-      },
-      {
-        text: 'Chat',
-        href: 'https://community.vuetifyjs.com',
-      },
-      {
-        text: 'Made with Vuetify',
-        href: 'https://madewithvuejs.com/vuetify',
-      },
-      {
-        text: 'Twitter',
-        href: 'https://twitter.com/vuetifyjs',
-      },
-      {
-        text: 'Articles',
-        href: 'https://medium.com/vuetify',
-      },
-    ],
-    whatsNext: [
-      {
-        text: 'Explore components',
-        href: 'https://vuetifyjs.com/components/api-explorer',
-      },
-      {
-        text: 'Select a layout',
-        href: 'https://vuetifyjs.com/layout/pre-defined',
-      },
-      {
-        text: 'Frequently Asked Questions',
-        href: 'https://vuetifyjs.com/getting-started/frequently-asked-questions',
-      },
-    ],
-  }),
+  data () {
+    return {
+      errors: [],
+      tasks: [
+        {
+          id: -1,
+          name: 'Study AWS',
+          isCompleted: false,
+          isImportant: true,
+          dueDate: null,
+          description: '',
+          group: 'Default',  
+        },
+        {
+          id: -2,
+          name: 'Buy laptop stand',
+          isCompleted: false,
+          isImportant: false,
+          dueDate: null,
+          description: '',
+          group: 'Default',  
+        }
+      ],
+      newTaskName: '',
+      counter: 0,
+    }
+  },
+  methods: {
+    createTask () {
+      if (!this.newTaskName) {
+        this.errors.push('Invalid task name');
+        return;
+      }
+
+      var newTask = {
+        id: this.counter++,
+        name: this.newTaskName,
+        isCompleted: false,
+        isImportant: false,
+        dueDate: null,
+        description: '',
+        group: 'Default',
+      }
+
+      this.tasks.push(newTask);
+      this.newTaskName = '';
+    }
+  }
 };
 </script>
