7ccba4db562ba1e5d4045698b5576d39bd665474
Ryan <jh3155@gmail.com>
2019-10-27 18:07:07 -0400

    taskdetail alignment fix

--- lets-do-it/src/components/TaskDetail.vue
+++ lets-do-it/src/components/TaskDetail.vue
@@ -1,59 +1,57 @@
 <template>
-  <v-container>
-    <v-list>
-      <v-list-item>
-        <v-text-field
-          v-model="newTaskName"
-          label="Task"
-          @keydown.enter="updateTask"
-        ></v-text-field>
-      </v-list-item>
+  <v-list>
+    <v-list-item>
+      <v-text-field
+        v-model="newTaskName"
+        label="Task"
+        @keydown.enter="updateTask"
+      ></v-text-field>
+    </v-list-item>
 
-      <v-list-item>
-        <v-switch v-model="task.isCompleted" :label="`Completed`"></v-switch>
-      </v-list-item>
+    <v-list-item>
+      <v-switch v-model="task.isCompleted" :label="`Completed`"></v-switch>
+    </v-list-item>
 
-      <v-list-item>
-        <v-switch v-model="task.isImportant" :label="`Important`"></v-switch>
-      </v-list-item>
+    <v-list-item>
+      <v-switch v-model="task.isImportant" :label="`Important`"></v-switch>
+    </v-list-item>
 
-      <v-list-item>
-        <v-menu
-          ref="displayDueDateDatePicker"
-          v-model="displayDueDateDatePicker"
-          :close-on-content-click="false"
-          :return-value.sync="task.dueDate"
-          transition="scale-transition"
-          offset-y
-          min-width="290px"
-        >
-          <template v-slot:activator="{ on }">
-            <v-text-field
-              v-model="task.dueDate"
-              label="Due Date"
-              prepend-icon="event"
-              readonly
-              v-on="on"
-            ></v-text-field>
-          </template>
-          <v-date-picker v-model="task.dueDate" no-title scrollable>
-            <v-spacer></v-spacer>
-            <v-btn text color="primary" @click="displayDueDateDatePicker = false">Cancel</v-btn>
-            <v-btn text color="primary" @click="$refs.displayDueDateDatePicker.save(task.dueDate)">OK</v-btn>
-          </v-date-picker>
-        </v-menu>
-      </v-list-item>
+    <v-list-item>
+      <v-menu
+        ref="displayDueDateDatePicker"
+        v-model="displayDueDateDatePicker"
+        :close-on-content-click="false"
+        :return-value.sync="task.dueDate"
+        transition="scale-transition"
+        offset-y
+        min-width="290px"
+      >
+        <template v-slot:activator="{ on }">
+          <v-text-field
+            v-model="task.dueDate"
+            label="Due Date"
+            prepend-icon="event"
+            readonly
+            v-on="on"
+          ></v-text-field>
+        </template>
+        <v-date-picker v-model="task.dueDate" no-title scrollable>
+          <v-spacer></v-spacer>
+          <v-btn text color="primary" @click="displayDueDateDatePicker = false">Cancel</v-btn>
+          <v-btn text color="primary" @click="$refs.displayDueDateDatePicker.save(task.dueDate)">OK</v-btn>
+        </v-date-picker>
+      </v-menu>
+    </v-list-item>
 
-      <v-list-item>
-        <v-textarea
-          v-model="newNotes"
-          outlined
-          label="Notes"
-          @keyup="throttledUpdateTask"
-        ></v-textarea>
-      </v-list-item>
-    </v-list>
-  </v-container>
+    <v-list-item>
+      <v-textarea
+        v-model="newNotes"
+        outlined
+        label="Notes"
+        @keyup="throttledUpdateTask"
+      ></v-textarea>
+    </v-list-item>
+  </v-list>
 </template>
 
 <script>
