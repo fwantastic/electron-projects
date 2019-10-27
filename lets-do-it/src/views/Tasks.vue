4998c25dd2a5dbfd0ea2cd4036633759eb2a89af
Ryan <jh3155@gmail.com>
2019-10-26 22:36:46 -0400

    added task detail view

--- lets-do-it/src/views/Tasks.vue
+++ lets-do-it/src/views/Tasks.vue
@@ -1,58 +1,75 @@
 <template>
   <v-container>
-    <v-text-field
-      v-model="newTaskName"
-      label="Create a task !"
-      outlined
-      @keydown.enter="createTask"
-    ></v-text-field>
+    <v-row>
+      <v-col cols="12" sm="8">
+        <v-list>
+          <v-subheader>TASKS</v-subheader>
 
-    <v-list>
-      <v-subheader>TASKS</v-subheader>
+          <v-list-item>
+            <v-text-field
+              v-model="newTaskName"
+              label="Add a task !"
+              outlined
+              @keydown.enter="createTask"
+            ></v-text-field>
+          </v-list-item>
 
-      <v-list-item
-        v-for="task in tasks"
-        :key="task.id"
-        @click="createTask"
-      >
-        <template>
-          <v-list-item-action>
-            <v-checkbox
-              v-model="task.isCompleted"
-              color="success"
-            ></v-checkbox>
-          </v-list-item-action>
-
-          <v-list-item-content>
-            <v-list-item-title v-text="task.name"></v-list-item-title>
-            <v-list-item-subtitle v-text="task.isImportant"></v-list-item-subtitle>
-          </v-list-item-content>
-
-          <v-list-item-action
-            @click="task.isImportant = !task.isImportant"
+          <v-list-item
+            v-for="task in tasks"
+            :key="task.id"
+            @click="selectTask(task)"
           >
-            <v-icon
-              v-if="task.isImportant"
-              color="yellow"
-            >
-              star
-            </v-icon>
+            <template>
+              <v-list-item-action>
+                <v-checkbox
+                  v-model="task.isCompleted"
+                  color="success"
+                ></v-checkbox>
+              </v-list-item-action>
 
-            <v-icon
-              v-else
-              color="grey lighten-1"
-            >
-              star_border
-            </v-icon>
-          </v-list-item-action>
-        </template> 
-      </v-list-item>
-    </v-list>
+              <v-list-item-content>
+                <v-list-item-title v-text="task.name"></v-list-item-title>
+                <v-list-item-subtitle v-text="task.description"></v-list-item-subtitle>
+                <v-list-item-subtitle v-text="task.group"></v-list-item-subtitle>
+              </v-list-item-content>
+
+              <v-list-item-action
+                @click="task.isImportant = !task.isImportant"
+              >
+                <v-icon
+                  v-if="task.isImportant"
+                  color="yellow"
+                >
+                  star
+                </v-icon>
+
+                <v-icon
+                  v-else
+                  color="grey lighten-1"
+                >
+                  star_border
+                </v-icon>
+              </v-list-item-action>
+            </template> 
+          </v-list-item>
+        </v-list>
+      </v-col>
+
+      <v-col sm="4">
+        <TaskDetail v-if="this.selectedTask != null" v-bind:task=this.selectedTask />
+      </v-col>
+    </v-row>
   </v-container>
+
 </template>
 
 <script>
+import TaskDetail from '@/components/TaskDetail';
+
 export default {
+  components: {
+    TaskDetail
+  },
   data () {
     return {
       errors: [],
@@ -63,7 +80,7 @@ export default {
           isCompleted: false,
           isImportant: true,
           dueDate: null,
-          description: '',
+          description: 'Need to find a room to study',
           group: 'Default',  
         },
         {
@@ -74,10 +91,20 @@ export default {
           dueDate: null,
           description: '',
           group: 'Default',  
+        },
+        {
+          id: -3,
+          name: 'Exercise!',
+          isCompleted: true,
+          isImportant: false,
+          dueDate: null,
+          description: '',
+          group: 'Default',  
         }
       ],
       newTaskName: '',
       counter: 0,
+      selectedTask: null,
     }
   },
   methods: {
@@ -99,6 +126,9 @@ export default {
 
       this.tasks.push(newTask);
       this.newTaskName = '';
+    },
+    selectTask(task) {
+      this.selectedTask = task;
     }
   }
 };
