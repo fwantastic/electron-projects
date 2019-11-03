0dc67f8a1fcdcf732accee0edce6b9839d847645
Ryan <jh3155@gmail.com>
2019-11-03 17:41:38 -0500

    added nedb

--- lets-do-it/src/main.js
+++ lets-do-it/src/main.js
@@ -2,8 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import vuetify from './plugins/vuetify';
 import router from './router'
+import { remote } from 'electron'
 
 Vue.config.productionTip = false
+
+Vue.prototype.$db = remote.getGlobal('db');
 
 new Vue({
   vuetify,
